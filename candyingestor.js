d499531fa0ed58e000770f341ed356d5e35b0bfe
Cole <jocowig@gmail.com>
2016-12-01 23:27:01 -0500

    Added Get and Post methods with placeholders
    Created Restify Get and Post methods with placeholders for the
    collection and data.

--- candyingestor.js
+++ candyingestor.js
@@ -1,9 +1,36 @@
 var restify = require('restify');
 var	port = process.env.PORT || 3000;	
+
 var server = restify.createServer({
 	name: 'Candy Ingestor'
 });
 
-server.listen(port, () => {
-	console.info('Server is running on port ' + port);
+//placeholder collection
+var placeholderCollection = {};
+var idNum = 0;
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.bodyParser());
+
+server.get('/', function(req, res, next){
+	res.setHeader('content-type', 'application/json');
+	res.writeHead(200);
+	res.end(JSON.stringify(placeholderCollection));
+	return next();
 });
+
+server.post('/placeholder', function(req, res, next){
+	var placeholder = req.params;
+	idNum++;
+	placeholder.id = idNum;
+	placeholderCollection[placeholder.id] = placeholder;
+	res.setHeader('content-type', 'application/json');
+	res.writeHead(200);
+	res.end(JSON.stringify(placeholder));
+	return next();
+});
+
+server.listen(port, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
