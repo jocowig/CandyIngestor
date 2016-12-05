0b85cb913a3cc691a8cc7714a8064270e084aebb
Cole <jocowig@gmail.com>
2016-12-04 23:09:56 -0500

    Implemented csv Parser, Moved data to tables in home
    ParseCSV now works with csv files. Database data is displayed in table
    format on home tab. Error checking and duplication checking not yet
    implemented. Major code cleanup still neaded

--- views/js/main.js
+++ views/js/main.js
@@ -12,28 +12,79 @@ function retrieveJSON(){
 }
 
 populateCompanies = function(companies){
-	var list = document.getElementById('companyList');
+	var companyTableSpace = document.getElementById('companyTable');
+	companyTableSpace.appendChild(document.createTextNode("Companies:"));
+	var companyTable = document.createElement("table"); 
+	
 	companies.data.forEach(function(company){
-		var entry = document.createElement('li');
-		entry.appendChild(document.createTextNode(company.company+" "));
-		entry.appendChild(document.createTextNode(company.first_name+" "));
-		entry.appendChild(document.createTextNode(company.last_name+" "));
-		entry.appendChild(document.createTextNode(company.web_address+" "));
-		list.appendChild(entry);
+		var companyRow = companyTable.insertRow(-1);
+		companyRow.insertCell(-1).innerHTML = company.company;
+		companyRow.insertCell(-1).innerHTML = company.web_address;
+		companyRow.insertCell(-1).innerHTML = company.first_name;
+		companyRow.insertCell(-1).innerHTML = company.last_name;
+		companyRow.insertCell(-1).innerHTML = company.phone_number;
+		companyRow.insertCell(-1).innerHTML = company.email;
+		companyRow.insertCell(-1).innerHTML = company.date;
+		companyRow.insertCell(-1).innerHTML = company.ip_address;
 	});
+	companyTableSpace.appendChild(companyTable);
 }
 
 populateCandies = function(candyItems){
-	var list = document.getElementById('companyList');
+	var candyTableSpace = document.getElementById('candyItemsTable');
+	candyTableSpace.appendChild(document.createTextNode("Candy Items:"));
+	var candyTable = document.createElement("table"); 
 	candyItems.data.forEach(function(candyItem){
-		var entry = document.createElement('li');
-		entry.appendChild(document.createTextNode(candyItem.company+" "));
-		entry.appendChild(document.createTextNode(candyItem.candy_one+" "));
-		entry.appendChild(document.createTextNode(candyItem.candy_two+" "));
-		entry.appendChild(document.createTextNode(candyItem.candy_three+" "));
-		entry.appendChild(document.createTextNode(candyItem.web_address+" "));
-		list.appendChild(entry);
+		var candyRow = candyTable.insertRow(-1);
+		candyRow.insertCell(-1).innerHTML = candyItem.company;
+		candyRow.insertCell(-1).innerHTML = candyItem.web_address;
+		candyRow.insertCell(-1).innerHTML = candyItem.candy_one;
+		candyRow.insertCell(-1).innerHTML = candyItem.candy_two;
+		candyRow.insertCell(-1).innerHTML = candyItem.candy_three;
 	});
+	candyTableSpace.appendChild(candyTable);
+}
+
+function Upload() {
+	var outputArray = {};
+	var fileUpload = document.getElementById("fileUpload");
+	var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+    if (regex.test(fileUpload.value.toLowerCase())) {
+        if (typeof (FileReader) != "undefined") {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var table = document.createElement("table");
+                var rows = e.target.result.split("\n");
+				var keys = rows[0].split(",")
+                for (var i = 1; i < rows.length; i++) {
+                    var row = table.insertRow(-1);
+                    var cells = rows[i].split(",");
+                    for (var j = 0; j < cells.length; j++) {
+                       var cell = row.insertCell(-1);
+                       cell.innerHTML = cells[j];
+						outputArray[keys[j]] = cells[j];
+					}
+					$.ajax({
+					  url: 'http://localhost:3000/company',
+					  data: outputArray,
+					  type: 'post',
+					  success: function(data) {
+						
+					  }
+					});
+                }
+				//alert(“Ingestion and data transfer process successfully completed!”);
+                var dvCSV = document.getElementById("dvCSV");
+                dvCSV.innerHTML = "";
+                dvCSV.appendChild(table);
+			}
+            reader.readAsText(fileUpload.files[0]);
+        } else {
+            alert("This browser does not support HTML5.");
+        }
+    } else {
+        alert("Please upload a valid CSV file.");
+    }
 }
 
 function openTab(evt, tabName) {
